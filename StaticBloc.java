dd4b6648541e09a8f43c81b30adbb4ef41601015
iosiftamas <iosiftamas@yahoo.com>
2020-06-24 14:25:25 +0300

    dece cu final param nu se executa static iar  fara final se executa?

--- StaticBloc.java
+++ StaticBloc.java
@@ -10,7 +10,7 @@ public class StaticBloc {
 
 class Demo {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {graea
         System.out.println(StaticBloc.param);
     }
 }
